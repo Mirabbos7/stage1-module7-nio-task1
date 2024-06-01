fc52fe39c5772d335996d2d1f3c14cb6e261594a
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-06-01 15:27:08 +0500

    Not finished yet

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        Profile profile = fileReader.getDataFromFile(new File("src/main/resources/Profile.txt"));
+        System.out.println(profile);
+    }
 
     public Profile getDataFromFile(File file) {
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
+            FileChannel inChanel = randomAccessFile.getChannel();
+            {
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                while (inChanel.read(byteBuffer) > 0) {
+                    byteBuffer.flip();
+                    for (int i = 0; i < byteBuffer.limit(); i++) {
+                        System.out.print((char) byteBuffer.get());
+                    }
+                    byteBuffer.clear();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return new Profile();
     }
 }
